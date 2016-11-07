193285f2da25ee38cc41137bb065d5ed0ec6e533
shangguan <1375304121@qq.com>
2016-11-07 09:39:51 +0800

    生成订单php代码

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -2,27 +2,14 @@
 if (! defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class User_model extends MY_Model
+class Order_model extends MY_Model
 {
-    protected $_tbName = 'users';
+    protected $_tbName = 'order';
     protected $primaryKey = 'id';
 
     public function __construct ()
     {
-    	parent::__construct();
-    }
-    
-    public function get_user_info(){
-        $this->db->select('a.*,b.name province_name,c.name city_name,d.name area_name')->from('users a');
-        $this->db->join('province b','a.province_code = b.code','left');
-        $this->db->join('city c','a.city_code = c.code','left');
-        $this->db->join('area d','a.area_code = d.code','left');
-        $this->db->where('a.id',$this->session->userdata('uid'));
-        return $this->db->get()->row_array();
-    }
-
-    public function get_user($user_flag){
-        return $result = $this->db->select('*')->from('users')->where("id = $user_flag or mobile = $user_flag")->get()->row_array();
+        parent::__construct();
     }
 
     function queryByKey($id, $selectFields = '*') {
