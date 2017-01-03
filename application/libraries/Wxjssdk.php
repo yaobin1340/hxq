ee789fe3ce9363c97f5fcb78c05ef661673ecd62
yaobin1340 <645894453@qq.com>
2017-01-03 20:25:45 +0800

    fix

--- application/libraries/Wxjssdk.php
+++ application/libraries/Wxjssdk.php
@@ -19,6 +19,8 @@ class Wxjssdk {
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
         $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
+        die($url);
+
         $timestamp = time();
         $nonceStr = $this->wxcreateNonceStr();
 
