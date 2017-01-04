91474b39c123b74ecb0cb588a4585f8d35a8ab0f
ksyydream <249499109@qq.com>
2017-01-04 20:27:31 +0800

    openid

--- application/models/Apishop_model.php
+++ application/models/Apishop_model.php
@@ -230,6 +230,7 @@ class Apishop_model extends MY_Model
             'mobile'=>$user_info->mobile,
             'price'=>$price,
             'shop_id'=>$shop_id,
+            'GW'=>$order_GW,
             'status'=>1,
             'oid'=>$oid,
             'cdate'=>date('Y-m-d H:i:s',time())
