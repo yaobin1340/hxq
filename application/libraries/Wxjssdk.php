8cf366b1c23ecd93df3731afeeeb2ac70650e78e
yaobin1340 <645894453@qq.com>
2017-01-05 16:03:51 +0800

    fix

--- application/libraries/Wxjssdk.php
+++ application/libraries/Wxjssdk.php
@@ -17,7 +17,7 @@ class Wxjssdk {
 
         // 注意 URL 一定要动态获取，不能 hardcode.
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url = $_SERVER['HTTP_REFERER'];
+        $url = "http://fd.jiangsuwxw.com";
         if($suffix)
             $url = $url.'/'.$suffix;
         if($_GET['shop_id'])
