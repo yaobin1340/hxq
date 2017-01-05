ffb3957c0dd61fa72a93421b316cec824f118886
yaobin1340 <645894453@qq.com>
2017-01-05 14:24:22 +0800

    fix

--- application/libraries/Wxjssdk.php
+++ application/libraries/Wxjssdk.php
@@ -17,7 +17,10 @@ class Wxjssdk {
 
         // 注意 URL 一定要动态获取，不能 hardcode.
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url = "http://fd.jiangsuwxw.com/".$suffix."?shop_id=59";
+        $url = "http://fd.jiangsuwxw.com/".$suffix;
+        if($_GET['shop_id'])
+            $url = $url."?shop_id=".$_GET['shop_id'];
+
 
         $timestamp = time();
         $nonceStr = $this->wxcreateNonceStr();
