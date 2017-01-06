871863d5501363e3c059d30ccfe0a2ba7a8b30d7
yaobin1340 <645894453@qq.com>
2017-01-06 15:11:30 +0800

    fix

--- application/controllers/Information.php
+++ application/controllers/Information.php
@@ -16,14 +16,23 @@ class Information extends CI_Controller
 
     public function index()
     {
+        echo '1';
         $data = $this->frontend_model->show_information();
+        echo '2';
         $ysday = $this->frontend_model->yesterday_info();
+        echo '3';
         $jukuan = $this->frontend_model->jukuan();
+        echo '4';
         $phangcity= $this->frontend_model->phang_city();
+        echo '5';
         $phangcompany= $this->frontend_model->phang_company();
+        echo '6';
         $lminfo = $this->frontend_model->lminfo();
+        echo '7';
         $this->cismatry->assign('lminfo', $lminfo);
+        echo '8';
         $this->cismatry->assign('phangcity', $phangcity);
+        echo '9';
         $this->cismatry->assign('phangcompany', $phangcompany);
         $this->cismatry->assign('jukuan', $jukuan);
         $this->cismatry->assign('data', $data);
